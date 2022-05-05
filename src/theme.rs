2ea9ea7376187f7170454ffc6e45b032d2d5dd67
Mike Grunweg <grunweg@posteo.de>
2022-05-05 20:33:52 +0200

    fix: handle very long items in clear_preserve_prompt correctly.
    The previous code would (correctly) handle the case of an item
    spanning between one and two lines, but failed to consider the case
    of the output spanning more than two. Extend the logic to cover that.

--- src/theme.rs
+++ src/theme.rs
@@ -856,9 +856,8 @@ impl<'a> TermThemeRenderer<'a> {
         let mut new_height = self.height;
         // Check each item size, increment on finding an overflow
         for size in size_vec {
-            if *size > self.term.size().1 as usize {
-                new_height += 1;
-            }
+            let term_width = self.term.size().1 as usize;
+            new_height += *size / term_width;
         }
 
         self.term.clear_last_lines(new_height)?;
